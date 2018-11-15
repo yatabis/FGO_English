04d34a827b6a4a18270406f23279492f7d640bef
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-11-15 11:45:50 +0900

    add: FlexMessage of section list

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from bottle import request, route, run
 import json
+import mysql.connector
 import os
 from pprint import pprint
 import requests
+from urllib.parse import parse_qs
 
 CAT = os.environ['CHANNEL_ACCESS_TOKEN']
 HEADER = {'Content-Type': 'application/json',
@@ -11,6 +13,9 @@ REPLY_EP = "https://api.line.me/v2/bot/message/reply"
 
 with open("main_story_carousel.json") as j:
     carousels = json.load(j)
+
+with open("section_list.json") as j:
+    section_list = json.load(j)
 
 
 def reply_message(token, messages):
@@ -32,28 +37,39 @@ def reply_text(token, text):
 
 
 CONTINUE = {
-   "type": "postback",
-   "label": "Continue",
-   "data": "action=continue",
+    "type": "postback",
+    "label": "Continue",
+    "data": "action=continue",
 }
 
 SKIP = {
-   "type": "postback",
-   "label": "Skip",
-   "data": "action=skip",
+    "type": "postback",
+    "label": "Skip",
+    "data": "action=skip",
 }
 
 CONFIRM = {
-  "type": "template",
-  "altText": "Story text",
-  "template": {
-      "type": "confirm",
-      "text": "text",
-      "actions": [
-          CONTINUE,
-          SKIP
-      ]
-  }
+    "type": "template",
+    "altText": "Story text",
+    "template": {
+        "type": "confirm",
+        "text": "text",
+        "actions": [
+            CONTINUE,
+            SKIP
+        ]
+    }
+}
+
+SECTION_BUTTON = {
+    'type': 'button',
+    'action': {
+        'type': 'postback',
+        'label': 'セクション名',
+        'data': 'section=1'
+    },
+    'height': 'sm',
+    'style': 'secondary'
 }
 
 
@@ -64,14 +80,59 @@ def callback():
         reply_token = event['replyToken']
         pprint(event)
         if event['type'] == 'postback':
-            postback_data = event['postback']['data']
-            if postback_data == "main=1":
-                reply_message(reply_token, [carousels[0]])
-            elif postback_data == "main=1.5":
-                reply_text(reply_token, "ストーリー第1.5部の実装をお待ちください。")
-            elif postback_data == "main=2":
-                reply_text(reply_token, "ストーリー第2部の実装をお待ちください。")
+            postback_data = parse_qs(event['postback']['data'])
+            if "section" in postback_data:
+                pass
+            elif "chapter" in postback_data:
+                main = postback_data['main'][0]
+                chapter = postback_data['chapter'][0]
+                cursor.execute('select name from `main record` where `main record` = %s', [main])
+                part = cursor.fetchone()[0]
+                print(part)
+                cursor.execute(f'select name from `{part}` where chapter = %s', [chapter])
+                print(cursor.statement)
+                name = cursor.fetchone()[0]
+                print(name)
+                cursor.execute(f'check table {name}')
+                check = cursor.fetchone()[-1]
+                if check == "OK":
+                    cursor.execute(f'select title from {name}')
+                    section = cursor.fetchall()
+                    message = deepcopy(section_list)
+                    message['hero'][
+                        'url'] = f"https://raw.githubusercontent.com/yatabis/FGO_English/master/images/{name}.png"
+                    for sec in section:
+                        sec_btn = deepcopy(SECTION_BUTTON)
+                        sec_btn['action']['label'] = sec[0]
+                        message['body']['contents'].append(sec_btn)
+                else:
+                    if chapter == "F":
+                        print(f"ストーリー第{main}部序章の実装をお待ちください。")
+                    elif chapter == "FIN":
+                        print(f"ストーリー第{main}部終章の実装をお待ちください。")
+                    else:
+                        print(f"ストーリー第{main}部第{chapter}章の実装をお待ちください。")
+            elif "main" in postback_data:
+                main = postback_data['main'][0]
+                cursor.execute('select name from `main record` where `main record` = %s', [main])
+                name = cursor.fetchone()[0]
+                cursor.execute(f'check table `{name}`')
+                check = cursor.fetchone()[-1]
+                if check == "OK":
+                    reply_message(reply_token, [carousels[main]])
+                else:
+                    reply_text(reply_token, f"ストーリー第{main}部の実装をお待ちください。")
+            else:
+                reply_text(reply_token, "不正なリクエストが送信されました。")
 
 
 if __name__ == '__main__':
+    connection_config = {
+        'user': os.environ['DB_USER'],
+        'password': os.environ['DB_PASS'],
+        'host': os.environ['DB_HOST'],
+        'database': os.environ['DB_NAME'],
+    }
+    connection = mysql.connector.connect(**connection_config)
+    cursor = connection.cursor(dictionary=True)
     run(host="0.0.0.0", port=int(os.environ.get('PORT', 443)))
