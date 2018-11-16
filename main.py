3442ee235b3c982fc1efb5da79efb31ee2688572
yatabis <39yatabis.rim8820xxx@gmail.com>
2018-11-16 15:39:35 +0900

    add: loading stroy text

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def get_name(part=None, chapter=None, section=None):
     if section:
         part = main_record[main_record['main record'] == float(part)]['name'].item().lower().replace(" ", "_")
         chapter = eval(part)[eval(part)['chapter'] == chapter]['name'].item().replace(" ", "_")
-        name = eval(chapter)[eval(chapter)['section'] == section]['name'].item().lower()
+        name = eval(chapter)[eval(chapter)['section'] == int(section)]['name'].item().lower()
     elif chapter:
         part = main_record[main_record['main record'] == float(part)]['name'].item().lower().replace(" ", "_")
         name = eval(part)[eval(part)['chapter'] == chapter]['name'].item()
